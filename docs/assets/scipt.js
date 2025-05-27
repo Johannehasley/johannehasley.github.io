9420a8ca3913c9f4f87cfa3ec354ceea00c5d0a9
unknown <johannehasleydiessongo@gmail.com>
2025-05-27 23:29:25 +0100

    Déplace les fichiers dans /docs pour Github pages

---
+++ docs/assets/scipt.js
@@ -0,0 +1,18 @@
+// Menu Burger
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+});
+
+// Effet de défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
